532780ee08c69a5f1e0248073e9dfae82416a65a
wojeksol <wojeksol@gmail.com>
2025-02-08 15:18:15 +0000

    py-1

---
+++ python JG/Zadania_mg.py
@@ -0,0 +1,24 @@
+## Zadaanie 1
+
+a = int(input("Podaj długość boku a: "))
+b = int(input("Podaj długość boku b: "))
+
+print("Pole prostokąta wynosi:",a * b)
+
+# # Zadanie 2
+
+print("Program oblicza kwadrat i sześcian podaj przez uzytkownika liczby rzeczywistej\n");
+
+x = float(input("Podaj liczbe: "))
+
+print("Wynik kwadratu to",x ** 2 )
+
+print("Wynik sześcianu",x ** 3)
+
+# Zadanie 3
+
+print("Program oblica droge S przez pojazd poruszający się w czsie t i prędkość v")
+
+v, t = float(input("Podaj średnią prędkość poruczania w kilometrach (km/h):\n")), float(input("Podaj czas podróży w godzinach (h):\n"))
+
+print("Droga wynosi :",v * t)
